856f66168250a9b79f101cc4ce71c7b1dc9659a6
abenable <ableabenaitwe@gmail.com>
2025-01-27 12:39:26 +0300

    Normalize SQL query for retrieving column information of the Books table

--- task_4.sql
+++ task_4.sql
@@ -1,6 +1,6 @@
 USE alx_book_store;
 
-SELECT column_name, column_type, is_nullable, column_key, column_default, extra
-FROM information_schema.columns
-WHERE table_name = 'Books'
-AND table_schema = 'alx_book_store';
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'Books'
+AND TABLE_SCHEMA = 'alx_book_store';
